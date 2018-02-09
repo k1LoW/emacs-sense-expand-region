a4fe06215ceeb5ba55765349da7d787b1d235f44
k1LoW <k1lowxb@gmail.com>
2018-02-09 20:44:27 +0900

    Remove inline-string-rectangle

--- sense-expand-region.el
+++ sense-expand-region.el
@@ -1,7 +1,7 @@
-;;;sense-expand-region.el --- 'sense-region.el like' expand-region.el wrapper
+;;; sense-expand-region.el --- 'sense-region.el like' expand-region.el wrapper
 ;; -*- Mode: Emacs-Lisp -*-
 
-;; Copyright (C) 2012 by 101000code/101000LAB
+;; Copyright (C) 2012-2018 by 101000code/101000LAB
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -17,7 +17,7 @@
 ;; along with this program; if not, write to the Free Software
 ;; Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 
-;; Version: 0.0.4
+;; Version: 0.0.5
 ;; Author: k1LoW (Kenichirou Oyama), <k1lowxb [at] gmail [dot] com> <k1low [at] 101000lab [dot] org>
 ;; URL: http://code.101000lab.org
 
@@ -46,22 +46,21 @@
 ;; expand-region.el
 ;; https://github.com/magnars/expand-region.el
 
-;;; Require
+;;; Code:
+
+;; require
 (require 'expand-region)
-(require 'inline-string-rectangle)
 
 (add-to-list 'er/try-expand-list 'ser/mark-whole-line)
 (add-to-list 'er/try-expand-list 'ser/mark-forward)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Variable ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defvar sense-expand-region-status nil)
 (defvar sense-expand-region-before-start nil)
 (defvar sense-expand-region-before-end nil)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Interactive Functions ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun sense-expand-region (arg)
   (interactive "p")
-  (setq sense-expand-region-status nil)
   (if (eq last-command this-command)
       (ser/sense-expand-region arg)
     (let* ((p1 (point))
@@ -70,8 +69,7 @@
       (setq sense-expand-region-before-end (max p1 p2))
       (if (not (region-active-p))
           (set-mark (point))
-        (setq sense-expand-region-status 'rectangle)
-        (inline-string-rectangle)))))
+        nil))))
 
 ;;;###autoload
 (defun ser/sense-expand-region (arg)
@@ -172,22 +170,6 @@
           (setq arg 0))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Utilities Functions ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defadvice kill-ring-save (around sense-expand-region-kill-ring-save disable)
-  (if (or (not sense-expand-region-status) (not mm/mirrors))
-      ad-do-it
-    (ser/kill-new))
-  (setq sense-expand-region-status nil))
-(ad-enable-advice 'kill-ring-save 'around 'sense-expand-region-kill-ring-save)
-(ad-activate 'kill-ring-save)
-
-(defadvice kill-region (around sense-expand-region-kill-region disable)
-  (if (or (not sense-expand-region-status) (not mm/mirrors))
-      ad-do-it
-    (ser/kill-region))
-  (setq sense-expand-region-status nil))
-(ad-enable-advice 'kill-region 'around 'sense-expand-region-kill-region)
-(ad-activate 'kill-region)
-
 (defun ser/mark-whole-line ()
   (interactive)
   (set-mark (save-excursion
@@ -210,36 +192,5 @@
                 (unless (> line-end (point))
                   (goto-char line-end))
                 (point)))))
-
-(defun ser/kill-new ()
-  (let ((killed-text "") (mirrors mm/mirrors) (first t))
-    (if (> (overlay-start (car mirrors)) (overlay-start mm/master))
-        (setq mirrors (append (list mm/master) (reverse mirrors)))
-      (setq mirrors (append mirrors (list mm/master))))
-    (dolist (mirror mirrors)
-      (setq killed-text (concat
-                         killed-text
-                         (unless first "\n")
-                         (buffer-substring (overlay-start mirror) (overlay-end mirror))))
-      (setq first nil))
-    (kill-new killed-text)
-    (mm/deactivate-region-and-clear-all)))
-
-(defun ser/kill-region ()
-  (let ((killed-text "") (mirrors mm/mirrors) (first t))
-    (if (> (overlay-start (car mirrors)) (overlay-start mm/master))
-        (setq mirrors (append (list mm/master) (reverse mirrors)))
-      (setq mirrors (append mirrors (list mm/master))))
-    (dolist (mirror mirrors)
-      (setq killed-text (concat
-                         killed-text
-                         (unless first "\n")
-                         (buffer-substring (overlay-start mirror) (overlay-end mirror))))
-      (setq first nil))
-    (dolist (mirror (reverse mirrors))
-      (delete-region (overlay-start mirror) (overlay-end mirror))
-      (setq first nil))
-    (kill-new killed-text)
-    (mm/deactivate-region-and-clear-all)))
 
 (provide 'sense-expand-region)
